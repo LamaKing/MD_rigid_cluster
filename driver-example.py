ffc3420960e7ad6dcd0d60612301c0da2e2ef79a
Andrea Silva <andreasilva759@gmail.com>
2021-03-09 23:36:00 +0100

    Simple example

--- driver-example.py
+++ driver-example.py
@@ -9,17 +9,17 @@ from tool_create_circles import create_cluster_circle
 from tool_create_hexagons import create_cluster_hex
 from MD_rigid_rototrasl import MD_rigid_rototrasl
 
-def create_cluster_sizeN(N1, N2, a1 = np.array([4.45, 0]), a2 = np.array([-2.225, 3.85381304684075]),
-                         clgeom_fname = "input_circ.hex", cluster_f = create_cluster_circle):
-    """What am I doing here?"""
+def cluster_inhex_Nl(N1, N2,  a1 = np.array([4.45, 0]), a2 = np.array([-4.45/2, 4.45*np.sqrt(3)/2]),
+                     clgeom_fname = "input_circ.hex", cluster_f = create_cluster_circle):
+    """Create input file in EP hex format for a cluster of Bravais size Nl"""
 
     clgeom_file = open(clgeom_fname, 'w') # Store it somewhere for MD function
-    with NamedTemporaryFile() as tmp: # write input on tempfile
+    with NamedTemporaryFile(prefix=clgeom_fname, suffix='tmp', delete=True) as tmp: # write input on tempfile
         tmp.write(bytes("%i %i\n" % (N1, N2), encoding = 'utf-8'))
         tmp.write(bytes("%15.10g %15.10g\n" % (a1[0], a1[1]), encoding = 'utf-8'))
         tmp.write(bytes("%15.10g %15.10g\n" % (a2[0], a2[1]), encoding = 'utf-8'))
         tmp.seek(0) # Reset 'reading needle'
-        pos = create_cluster(tmp.name, clgeom_file)[:,:2] # Pass name of tempfile to create function
+        pos = cluster_f(tmp.name, clgeom_file)[:,:2] # Pass name of tempfile to create function
     clgeom_file.close() # Close file so MD function can read it
 
     return pos, clgeom_fname
@@ -27,13 +27,17 @@ def create_cluster_sizeN(N1, N2, a1 = np.array([4.45, 0]), a2 = np.array([-2.225
 #------------------------------------------
 # INPUTS
 #------------------------------------------
-# AS: Reads inputs from json file, easier for simul driver in python
+# Reads inputs from json file. We will then modify suff we are interested in.
 with open(sys.argv[1]) as inj:
     inputs = json.load(inj)
+
+Nl = 3 # 'Bravais' size
 
 #------------------------------------------
 # SETUP SYSTEM
 #------------------------------------------
+
+#------- CLUSTER GEOM -----
 # define cluster shape
 clt_shape = 'hexagon'
 if clt_shape == 'circle':
@@ -44,67 +48,45 @@ else:
     raise ValueError("Symmetry %s not implemented" % symmetry)
 
 # Cluster lattice
-R = 4.45
-a1 = np.array([R, 0])
-a2 = np.array([-R/2, R*np.sqrt(3)/2])
+Rcl = 4.45
+a1 = np.array([Rcl, 0])
+a2 = np.array([-Rcl/2, Rcl*np.sqrt(3)/2])
 
-# Free system no barrier
-inputs['epsilon'] = 0
+clgeom_fname = "input_circ-Nl_%i.hex" % Nl
+pos, _ = cluster_inhex_Nl(Nl, Nl, a1=a1, a2=a2,
+                              clgeom_fname=clgeom_fname, cluster_f=create_cluster)
+N = pos.shape[0]
+inputs['cluster_hex'] = clgeom_fname
 
-# Torque and final angle
-inputs['T'] = 1000 # fN*Micron
-inputs['Nsteps'] = int(1e3)
+# Estimate of free cluster moving
+theta1 = inputs['T']/N**2 * inputs['Nsteps']*inputs['dt']
 
-#------------------------------------------
-# RUN SCALING
-#------------------------------------------
+#------- SUBSTRATE --------
+# We want to define barrier
+inputs['epsilon'] = 105
+
+#------- MD PARAMS --------
+# Torque enought to make it move
+inputs['T'] = 10*N # fN*Micron
+inputs['Nsteps'] = int(2e4)
+
+#--------------------------
+# RUN
+#--------------------------
+print("Start run of %i steps" % inputs['Nsteps'])
+print("Free cl should rotate %.2f deg" % (theta1))
+c_outf = "out-N_%i.dat" % N
 t0 = time() # Start global clock
-out_data = open("free_scaling.dat", 'w')
-for Ni in range(2,10):
+with open(c_outf, 'w') as c_out:
+    MD_rigid_rototrasl([inputs], outstream=c_out, info_fname="info-N_%i.json" % N)
 
-    #--------------------------
-    # SETUP
-    #--------------------------
-    print("On N", Ni)
-
-    #------- CLUSTER GEOM -----
-    clgeom_fname = "input_circ-Nl_%i.hex" % Ni
-    pos, _ = create_cluster_sizeN(Ni, Ni, a1=a1, a2=a2,
-                                  clgeom_fname=clgeom_fname, cluster_f=create_cluster)
-    N = pos.shape[0]
-    inputs['cluster_hex'] = clgeom_fname
-
-    # ------ LANGEVIN ---------
-    eta = 1 # [fKg/ms]
-    # CM translational viscosity
-    etat_eff = eta*N
-    # CM rotational viscosity.
-    etar_eff = eta*N**2
-
-    theta1 = inputs['T']/etar_eff * inputs['Nsteps']*inputs['dt']
-
-    # ------ SETUP INFO -------
-    input_fname = 'inputs-N_%i.json' % N
-    with open(input_fname, 'w') as outj:
-        json.dump(inputs, outj)
-
-    #--------------------------
-    # RUN
-    #--------------------------
-    print("Start run of %i steps" % inputs['Nsteps'])
-    print("Free cl should rotate %.2f deg" % (theta1))
-    c_outf = "out-N_%i.dat" % N
-    with open(c_outf, 'w') as c_out:
-        MD_rigid_rototrasl([input_fname], outstream=c_out, info_fname="info-N_%i.json" % N)
-
-    #--------------------------
-    # ANALYSIS
-    #--------------------------
-    data = pd.read_fwf(c_outf, infer_nrows=1e3)
-    theta_fin, omega_fin = data['06)angle'].tail(1), data['07)omega'].tail(1)
-    print("%10i %10i %20.15g %20.15g" % (N, Ni, theta_fin, omega_fin), file=out_data)
-    out_data.flush()
-    print("-------------------------------------\n") # Separate a bit steps
+#--------------------------
+# ANALYSIS
+#--------------------------
+data = pd.read_fwf(c_outf, infer_nrows=1e3)
+theta_fin, omega_fin = data['06)angle'].tail(1), data['07)omega'].tail(1)
+print("Final config"
+print("N %10i (Nl %10i) theta fin %20.15g omega fin %20.15g" % (N, Nl, theta_fin, omega_fin))
 
 #------------------------------
 out_data.close()
---
+++ example/MD_rigid_rototrasl.py
@@ -0,0 +1,1 @@
+../MD_rigid_rototrasl.py
