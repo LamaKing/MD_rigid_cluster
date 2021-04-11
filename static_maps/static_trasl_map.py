8036367cb407b107ad014f237fa7d9ded55c8f85
Andrea Silva <andreasilva759@gmail.com>
2021-04-11 23:42:36 +0200

    Update with external modules

--- static_maps/static_trasl_map.py
+++ static_maps/static_trasl_map.py
@@ -4,111 +4,15 @@ import sys, os, json, logging, multiprocessing
 import numpy as np
 from time import time
 from functools import partial
-from tempfile import NamedTemporaryFile
-from tool_create_circles import create_cluster_circle
-from tool_create_hexagons import create_cluster_hex
-
-# calcolates simple matrix for mapping clusters colloids into primitive cell and viceversa.
-def calc_matrices_square(R):
-    """Metric matrices of square lattice of spacing R"""
-    area = R*R
-    u     = np.array([[1,0], [0,1]])*R/area
-    u_inv = np.array([[1,0], [0,1]])*R
-    return u, u_inv
-
-def calc_matrices_triangle(R):
-    """Metric matrices of triangular lattice of spacing R"""
-    area = R*R*np.sqrt(3)/2.
-    # NN along y
-    #    u     = np.array([[1,0], [-1./2, sqrt(3)/2]])*R/area
-    #    u_inv = np.array([[sqrt(3)/2,0], [1/2,1]])*R
-    # NN along x (like tool_create_hex/circ)
-    u =     np.array([[np.sqrt(3)/2.,0.5], [0,1]])*R/area
-    u_inv = np.array([[1,-0.5],            [0.0, np.sqrt(3)/2.]])*R
-    return u, u_inv
-
-def calc_en_tan(pos, a, b, ww, epsilon, u, u_inv):
-    """Calculate energy and forces. Well is approximated with tanh function."""
-    en = 0
-    F = np.zeros((2))
-    # map to substrate cell
-    Xp = pos[:,0]*u[0,0] + pos[:,1]*u[0,1]
-    Yp = pos[:,0]*u[1,0] + pos[:,1]*u[1,1]
-    Xp -= np.floor(Xp + 0.5)
-    Yp -= np.floor(Yp + 0.5)
-    X = Xp*u_inv[0, 0] + Yp*u_inv[0, 1]
-    Y = Xp*u_inv[1, 0] + Yp*u_inv[1, 1]
-    R  = np.sqrt(X*X+Y*Y)
-    # energy inside flat bottom region
-    en = -np.sum(R <= a)*epsilon
-    # colloids inside the curve region
-    # mask and relative R
-    inside = np.logical_and(R<b, R>a)
-    Xin = X[inside]
-    Yin = Y[inside]
-    Rin = R[inside]
-    # calculation of energy and force. See X. Cao Phys. Rev. E 103, 1 (2021).
-    xx = (Rin-a)/(b-a) # Reduce coordinate rho in [0,1]
-    # energy
-    en += np.sum(epsilon/2.*(np.tanh((xx-ww)/xx/(1-xx))-1.))
-    # force F = - grad(E)
-    ff = (xx-ww)/xx/(1-xx)
-    ass = (np.cosh(ff)*(1-xx)*xx)*(np.cosh(ff)*(1-xx)*xx)
-    vecF = -epsilon/2*(xx*xx+ww-2*ww*xx)/ass
-    # Go from rho to r again
-    vecF /= (b-a)
-    # Project to x and y
-    F[0] = np.sum(vecF*Xin/Rin)
-    F[1] = np.sum(vecF*Yin/Rin)
-    return en, F
-
-def calc_en_gaussian(pos, sigma, epsilon, u, u_inv):
-    Xp = pos[:,0]*u[0,0] + pos[:,1]*u[0,1]
-    Yp = pos[:,0]*u[1,0] + pos[:,1]*u[1,1]
-    Xp -= np.floor(Xp + 0.5)
-    Yp -= np.floor(Yp + 0.5)
-    X = Xp*u_inv[0, 0] + Yp*u_inv[0, 1]
-    Y = Xp*u_inv[1, 0] + Yp*u_inv[1, 1]
-    R  = np.sqrt(X*X+Y*Y)
-    off = R != 0
-    en = -epsilon*np.sum(gaussian(R[off], 0, sigma))
-    FX = epsilon*gaussian(R[off],0,sigma) * (R[off] / np.power(sigma, 2.)) * X[off] / R[off]
-    FY = epsilon*gaussian(R[off],0,sigma) * (R[off] / np.power(sigma, 2.)) * Y[off] / R[off]
-    return en, [np.sum(FX), np.sum(FY)]
-
-def gaussian(x, mu, sig):
-    return np.exp(-np.power(x - mu, 2.) / (2 * np.power(sig, 2.))) / (2. * np.pi * np.power(sig, 2.))
-
-def cluster_inhex_Nl(N1, N2,  a1 = np.array([4.45, 0]), a2 = np.array([-4.45/2, 4.45*np.sqrt(3)/2]),
-                     clgeom_fname = "input_pos.hex", cluster_f = create_cluster_circle):
-    """Create input file in EP hex format for a cluster of Bravais size Nl"""
-
-    clgeom_file = open(clgeom_fname, 'w') # Store it somewhere for MD function
-    with NamedTemporaryFile(prefix=clgeom_fname, suffix='tmp', delete=True) as tmp: # write input on tempfile
-        tmp.write(bytes("%i %i\n" % (N1, N2), encoding = 'utf-8'))
-        tmp.write(bytes("%15.10g %15.10g\n" % (a1[0], a1[1]), encoding = 'utf-8'))
-        tmp.write(bytes("%15.10g %15.10g\n" % (a2[0], a2[1]), encoding = 'utf-8'))
-        tmp.seek(0) # Reset 'reading needle'
-        pos = cluster_f(tmp.name, clgeom_file)[:,:2] # Pass name of tempfile to create function
-    clgeom_file.close() # Close file so MD function can read it
-
-    return pos, clgeom_fname
-
-# rotates pos vector (the first two rows are X,Y) by an angle in degrees
-def rotate(pos, angle):
-    for i in range(pos.shape[0]):
-        newx = pos[i,0] * np.cos(angle/180*np.pi) - pos[i,1] * np.sin(angle/180*np.pi)
-        newy = pos[i,0] * np.sin(angle/180*np.pi) + pos[i,1] * np.cos(angle/180*np.pi)
-        pos[i,0] = newx
-        pos[i,1] = newy
-    return pos
+from tool_create_cluster import create_cluster_hex, create_cluster_circle, rotate, cluster_inhex_Nl
+from tool_create_substrate import gaussian, calc_matrices_triangle, calc_matrices_square, calc_en_gaussian, calc_en_tan
 
 def static_traslmap_Nl(Nl, inputs, calc_en_f, name=None, out_fname=None, info_fname=None, debug=False):
 
     if name == None:
         name = 'traslmap_Nl_%i' % Nl
     if out_fname == None: out_fname = 'out-%s.dat' % name
-    if info_fname == None: info_fname = 'info-%s.txt' % name
+    if info_fname == None: info_fname = 'info-%s.json' % name
 
     #-------- SET UP LOGGER -------------
     # For this threads and children
@@ -160,15 +64,17 @@ def static_traslmap_Nl(Nl, inputs, calc_en_f, name=None, out_fname=None, info_fn
     inputs['cluster_hex'] = clgeom_fname
 
     # initialise variable
+    try:
+        pos_cm = np.array(inputs['pos_cm']) # Start pos [micron]
+    except KeyError:
+        pos_cm = np.zeros(2) # If not given, start from centre
     forces = np.zeros(2)
-    torque = 0.
-    da1 = np.zeros(2)
-    da2 = np.zeros(2)
-
+    torque = 0
     # Map params
     nbin = inputs['nbin'] # Reduce coordinate fraction
+    da1 = np.zeros(2)
+    da2 = np.zeros(2)
     Nsteps = nbin**2
-    dtheta = inputs['dtheta'] # Reduce coordinate fraction
 
     #-------- INFO FILE ---------------
     with open(info_fname, 'w') as infof:
@@ -208,17 +114,11 @@ def static_traslmap_Nl(Nl, inputs, calc_en_f, name=None, out_fname=None, info_fn
     it = 0
     for dda1 in np.linspace(0, 1, nbin, endpoint=True):
         for dda2 in np.linspace(0, 1, nbin, endpoint=True):
-            # This is so damn inefficient
+            # this is so damn inefficient
             delta = np.array([dda1, dda2])
             r =  np.dot(delta, u_inv)
-            pos_cm[:] = r
-            #pos_cm[:] =  np.array([dda1, dda2])
-            # energy is -epsilon inside well, 0 outside well. Continuous function in between.
-            e_pot, forces = calc_en_f(pos + pos_cm, *en_params)
-            # torque is T = - dE / dTheta = - (E(theta+) - E(theta-)) / 2dTheta
-            en_plus, _ = calc_en_f(rotate(pos, dtheta) + pos_cm, *en_params)
-            en_minus,_ = calc_en_f(rotate(pos,-dtheta) + pos_cm, *en_params)
-            torque = -(en_plus - en_minus)/dtheta/2
+            pos_cm = r
+            e_pot, forces, torque = calc_en_f(pos + pos_cm, pos_cm, *en_params)
 
             # Print progress
             if it % printerr_skip == 0:
@@ -252,6 +152,8 @@ if __name__ == "__main__":
 
     with open(sys.argv[1]) as inj:
         inputs = json.load(inj)
+
+    N0, N1 = int(sys.argv[2]), int(sys.argv[3])
 
     # Substrate
     Rcl = 4.45 # Lattice spacing of cluster. Fixed by experiments.
@@ -288,9 +190,11 @@ if __name__ == "__main__":
 
     # Set up system for multiprocess
     ncpu = os.cpu_count()
-    nworkers = 4
-    Nl_range = [4, 10] # Sizes to explore, in parallels
-    c_log.info("Running %i elements on %i processes on %i cores" % (len(Nl_range), nworkers, ncpu))
+    nworkers = 1
+    try: nworkers = int(sys.argv[4])
+    except: pass
+    Nl_range = range(N0, N1) # Sizes to explore, in parallels
+    c_log.info("Running %i elements on %i processes (%i cores machine)" % (len(Nl_range), nworkers, ncpu))
 
     # Fix the all arguments a part from Nl, so that we can use pool.map
     partial_traslmap_Nl = partial(static_traslmap_Nl, inputs=inputs, calc_en_f=calc_en_f, debug=debug) # defaut should be fine for most
